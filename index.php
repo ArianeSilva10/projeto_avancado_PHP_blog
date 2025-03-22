b9abb1bca52e4adbe5bb9f55ba99bc46c164cd71
ArianeSilva10 <arianedossantos31@gmail.com>
2025-03-22 10:19:58 -0300

    setup inicial

--- index.php
+++ index.php
@@ -9,9 +9,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
+
+    <!-- ESTILOS DO PROJETO -->
     <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Winky+Sans:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+
 </head>
 <body>
-    <h1>oi</h1>
+    <h1>Olá Mundo!</h1>
 </body>
 </html>
